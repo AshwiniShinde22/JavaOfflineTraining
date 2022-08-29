34716cfe22da030bef3993ad79e4ef6ce3d2e086
AshwiniShinde22 <111857188+AshwiniShinde22@users.noreply.github.com>
2022-08-29 17:33:35 +0530

    Add files via upload

---
+++ ques5/B.java
@@ -0,0 +1,14 @@
+package com.yash.assign2.ques5;
+
+public abstract class B extends A {
+
+	@Override
+	public int sub(int a, int b) {
+		return a=a-b;
+		
+	}
+
+	public abstract int mul(int a, int b);
+
+	
+}
---
+++ ques5/CalcAbs.java
@@ -0,0 +1,9 @@
+package com.yash.assign2.ques5;
+
+public abstract  class CalcAbs {
+	
+	public abstract int sum(int a, int b);
+
+	
+	
+}
