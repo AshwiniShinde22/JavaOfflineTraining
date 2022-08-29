1df3c6926610b2b4871d9efd7f776f0d7fdbec0f
AshwiniShinde22 <111857188+AshwiniShinde22@users.noreply.github.com>
2022-08-29 17:35:22 +0530

    Add files via upload

---
+++ ques8/Laptop.java
@@ -0,0 +1,12 @@
+package com.yash.assign2.ques8;
+
+public class Laptop extends Electornics
+{
+
+	public Laptop(int id, String semiconductorType, String dateOfManufacturing) {
+		super(id, semiconductorType, dateOfManufacturing);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ ques8/Test.java
@@ -0,0 +1,22 @@
+package com.yash.assign2.ques8;
+
+import java.util.logging.Logger;
+
+public class Test {
+
+	static Logger logger = Logger.getLogger(Test.class.getName());
+	public static void main(String[] args) {
+
+		Electornics electornics = new Electornics(101, "gallium", "03/01/1993");
+		Electornics lcd = new LCD(102, "crystal", "13/11/1994");
+		Electornics mobile = new Mobile(103, "Apple", "23/09/2021");
+		
+		logger.info("Electronics Info: "+electornics);
+		logger.info("LCD Info: "+lcd);
+		logger.info("Mobile Info: "+mobile);
+		
+		
+		
+	}
+
+}
