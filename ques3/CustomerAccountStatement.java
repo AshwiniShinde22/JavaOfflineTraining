d8c4a13da6547b34876b4d4285c9d39fd88bab80
AshwiniShinde22 <111857188+AshwiniShinde22@users.noreply.github.com>
2022-08-29 17:30:34 +0530

    Add files via upload

---
+++ ques3/CustomerAccountStatement.java
@@ -0,0 +1,56 @@
+package com.yash.assign2.ques3;
+
+public class CustomerAccountStatement extends Customer {
+	
+	
+	private int cAccId;
+	private int amount;
+	private int deposit_withdrawl;
+	private String deposit_date;
+	
+	
+	
+	public CustomerAccountStatement(int cAccId, int amount, int deposit_withdrawl, String deposit_date
+			) {
+		super(deposit_withdrawl, deposit_date, deposit_date, deposit_date, deposit_date, deposit_date);
+		this.cAccId = cAccId;
+		this.amount = amount;
+		this.deposit_withdrawl = deposit_withdrawl;
+		this.deposit_date = deposit_date;
+		
+	}
+	public int getcAccId() {
+		return cAccId;
+	}
+	public void setcAccId(int cAccId) {
+		this.cAccId = cAccId;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getDeposit_withdrawl() {
+		return deposit_withdrawl;
+	}
+	public void setDeposit_withdrawl(int deposit_withdrawl) {
+		this.deposit_withdrawl = deposit_withdrawl;
+	}
+	public String getDeposit_date() {
+		return deposit_date;
+	}
+	public void setDeposit_date(String deposit_date) {
+		this.deposit_date = deposit_date;
+	}
+	@Override
+	public String toString() {
+		return "CustomerAccountStatement [cAccId=" + cAccId + ", amount=" + amount + ", deposit_withdrawl="
+				+ deposit_withdrawl + ", deposit_date=" + deposit_date + "]";
+	}
+	
+	
+	
+	
+
+}
